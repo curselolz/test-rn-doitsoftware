0038f8627dd36cc75ceb8bc870667b7935f3e359
oleksandr <wogabe9685@seomail.top>
2020-02-05 12:32:53 +0200

    new package

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,10 @@
  */
 
 module.exports = {
+  resolver: {
+    /* resolver options */
+    sourceExts: ['jsx', 'js', 'tsx', 'ts']
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
